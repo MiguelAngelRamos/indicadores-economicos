bfd8cbf915974ec2c50c4d0f3121a5b5f03fe85c
Miguel Ramos <return.infox@gmail.com>
2019-12-10 00:44:13 -0300

    primera interfaz

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,90 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <select  v-model="indicador" class="form-control form-control-lg" name="indicador">
+        <option v-for="ind in indicadoresEconomicos" v-bind:key="ind.codigo">
+          {{ind.codigo  }}
+        </option>
+      </select>
+<spinner v-show="loading"/>
+  <table v-show="indicador != '' && !loading" class="container table mt-2">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">Fecha</th>
+        <th scope="col">Valor</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="d in datos.serie" :key="d.fecha">
+        <th scope="row">{{d.fecha | fecha}}</th>
+        <td>{{d.valor}}</td>
+    </tr>
+    </tbody>
+  </table>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import getIndicadores from './api';
+import Spinner from './components/Spinner.vue';
 
 export default {
   name: 'app',
+  data(){
+    return{
+      datos:[],
+      indicadoresEconomicos : [
+        { codigo : 'uf'},
+        { codigo : 'ivp'},
+        { codigo: 'dolar'},
+        { codigo: 'dolar_intercambio'},
+        { codigo: 'euro'},
+        { codigo: 'ipc'},
+        { codigo: 'utm'},
+        { codigo: 'imacec'},
+        { codigo: 'tpm'},
+        { codigo: 'libra_cobre'},
+        { codigo: 'tasa_desempleo'},
+        { codigo: 'bitcoin'}
+  ],
+  indicador:'',
+  loading:false,
+  indice: Date.now()
+    }
+  },
   components: {
-    HelloWorld
+    Spinner
+  },
+  methods:{
+    refreshIndicadores(){
+      const self = this;
+      this.loading = true
+      this.datos=[]
+      getIndicadores(this.indicador)
+        .then(function(indicadores){
+          self.loading = false
+          self.datos = indicadores
+          })
+    }
+  },
+  mounted(){
+    this.refreshIndicadores();
+  },
+  watch:{
+    indicador(){
+      this.refreshIndicadores();  
+    },
+    datos(){
+
+    }
+  },
+  filters: {
+    fecha: function (value) {
+      let f = new Date(value);
+      let fecha = `${f.getDate()}/${f.getMonth() + 1}/${f.getFullYear()}`;
+      return fecha
   }
+}
 }
 </script>
 
@@ -23,6 +95,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10  px;
 }
 </style>
